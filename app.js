9e4fcd06bcc400516618ec95955ec209ed020148
Jordy51 <adityakumawat51@gmail.com>
2021-02-23 09:51:03 +0530

    Database and Heroku app created

--- app.js
+++ app.js
@@ -4,4 +4,8 @@ const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+	res.send("This is project assignment fot MuxEmail.");
+});
+
 app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`));
