37668a94983d2218a23dbbae7ee7d4314faf3bfe
Jordy51 <adityakumawat51@gmail.com>
2021-02-25 16:08:29 +0530

    Database connected

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+require("dotenv").config();
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-	res.send("This is project assignment fot MuxEmail.");
-});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+// DB Config
+const db = process.env.MongoURI;
+
+// Connect MongoDB
+mongoose
+	.connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+	.then(() => console.log("MongoDB Connected!"))
+	.catch((err) => console.log(err));
+
+app.use("/", require("./routes/customerRoute"));
+	
 app.listen(PORT, () => console.log(`Server is up and running on port ${PORT}`));
